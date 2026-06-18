78f7e337b5e1b1e61b6d023455db41fbc789cd42
ColeStrickler <colestrickler@gmail.com>
2026-06-18 14:59:45 -0500

    shove

---
+++ src/main.cpp
@@ -0,0 +1,170 @@
+
+#include <fstream>
+#include <string.h>
+#include <chrono>
+#include "errors.hpp"
+#include "scanner.hpp"
+#include "frontend.hh"
+#include "name_analysis.hpp"
+#include "ast_transform_pass.hpp"
+#include "resource_analysis_pass.hpp"
+#include "resource_alloc_pass.hpp"
+#include "dtl_api.hpp"
+#include "optimizations/optimization_passes.hpp"
+
+
+static void writeTokenStream(const char * inPath, const char * outPath){
+	std::ifstream inStream(inPath);
+	if (!inStream.good()){
+		std::string msg = "Bad input stream";
+		msg += inPath;
+		throw new DTL::InternalError(msg.c_str());
+	}
+	if (outPath == nullptr){
+		std::string msg = "No tokens output file given";
+		throw new DTL::InternalError(msg.c_str());
+	} 
+
+	DTL::Scanner scanner(&inStream);
+	if (strcmp(outPath, "--") == 0){
+		scanner.outputTokens(std::cout);
+	} else {
+		std::ofstream outStream(outPath);
+		if (!outStream.good()){
+			std::string msg = "Bad output file ";
+			msg += outPath;
+			throw new DTL::InternalError(msg.c_str());
+		}
+		scanner.outputTokens(outStream);
+		outStream.close();
+	}
+}
+
+static DTL::ProgramNode * parse(const char * inFile){
+	std::ifstream inStream(inFile);
+	if (!inStream.good()){
+		std::string msg = "Bad input stream ";
+		msg += inFile;
+		throw new DTL::InternalError(msg.c_str());
+	}
+
+	//This pointer will be set to the root of the
+	// AST after parsing
+	DTL::ProgramNode * root = nullptr;
+    DTL::Scanner scanner(&inStream);
+	DTL::Parser parser(scanner, &root);
+	
+
+	int errCode = parser.parse();
+	
+	if (errCode != 0){ printf("parse() errCode: %d\n", errCode); return nullptr; }
+
+	return root;
+}
+
+
+
+
+
+
+
+#include <stdio.h>
+std::string FileToString(const std::string& file_)
+{
+  std::ifstream file(file_);
+    if (!file) {
+        std::cerr << "Failed to open file\n";
+        return "";
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();  // Read entire file into the buffer
+    std::string contents = buffer.str();
+    return contents;
+}
+
+
+
+int main()
+{
+	//auto hwStat = new DTL::AGUHardwareStat(4, 4, 5, 6, 5, 4, 9, 1, 2);
+	//auto hwStat = new DTL::AGUHardwareStat(4, 4, 5, 6, 5, 4, 8, 1, 2);
+	//auto hwStat = new DTL::AGUHardwareStat(3, 4, 5, 6, 5, 4, 1, 1, 2);
+
+    auto hwStat = new DTL::AGUHardwareStat({ // (nAdd, nMult, nSUb, nPassThru) 
+      {2,4,2,4},
+      {2,2,1,4},
+      {2,2,1,2},
+      {1,1,1,2},
+      {1,1,1,1},
+      {0,0,0,1},
+    }, 5, 6, 7, 2, 1, 2);
+    hwStat->nMaxConfigs = 4;
+
+	//hwStat->bytesCell = 2; // maxVarOutputs = 2
+
+    std::istringstream input(FileToString("./test.dtl"));
+    DTL::ProgramNode * root = nullptr;
+    DTL::Scanner scanner(&input);
+	DTL::Parser parser(scanner, &root);
+    //parser.set_debug_level(1);  // Turn on debugging
+    int err = parser.parse();
+	if (err != 0){ printf("parse() errCode: %d\n", err); return false; }
+
+    auto na = DTL::NameAnalysis::build(root);
+	if (na == nullptr) {
+
+		return false;
+	}
+	auto ta = DTL::TypeAnalysis::build(na);
+	if (ta == nullptr)
+	{
+
+        return false;
+    }
+	auto condInfo = ta->GetConditionalInfo();
+
+	root->PrintAST("out_untransform.ast");
+	printf("here\n");
+	
+
+	root = static_cast<DTL::ProgramNode*>(DTL::DTLOptimizer::OptimizeStatic(root, DTL::DTL_OPTLEVEL::OPTMAX));
+	root->PrintAST("out_constfold.ast");
+
+
+
+    root = static_cast<DTL::ProgramNode*>(DTL::ASTTransformPass::Transform(root, DTL_OPT_MAX));
+    if (root == nullptr)
+    {     
+
+        return false;
+    }
+	root->PrintAST("out.ast");
+		
+	auto ra = DTL::ResourceAnalysis::build(root, hwStat);
+    if (ra == nullptr)
+    {
+        //ERR("ResourceAnalysis Failed");
+        return false;
+    }
+    ra->GetResources()->GetNeededResourceStats();
+	auto rsrc_string = ra->GetResources()->toString();
+		
+	std::cout << rsrc_string << "\n";
+
+    auto ralloc = DTL::ResourceAllocation::build(ra, hwStat, condInfo);
+    if (ralloc == nullptr)
+    {
+        //ERR("ResourceAllocation failed\n");
+        return false;
+    }
+
+	ralloc->PrintControlWrites("regwrites.out", AGU_CONFIG_BASE);
+	ralloc->PrintInitStateRegisters("regwrites.out", AGU_CONFIG_BASE);
+	
+    printf("Successfully parsed!\n");
+    
+	
+	std::cout << rsrc_string << "\n";
+	return 0;
+}
